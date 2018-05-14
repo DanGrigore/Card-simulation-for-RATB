de9171629afde2d6b20f77e88b3d27163ed77611
Nadstrif <grigoredanandrei@yahoo.com>
2018-05-14 22:24:44 +0300

    update database

--- DataBases/DB_inserts.sql
+++ DataBases/DB_inserts.sql
@@ -22,8 +22,28 @@ DELETE FROM CARD WHERE card_id = 12;
 
 DELETE FROM VALIDATION WHERE id = 3;
 
-SELECT * FROM CARD;
 SELECT * FROM CLIENT;
+SELECT * FROM CARD;
 SELECT * FROM TRANSPORT;
 SELECT * FROM CARD_TYPE;
 SELECT * FROM VALIDATION;
+
+SELECT * FROM CLIENT c, CARD  cs
+where c.client_id = cs.client_id AND c.first_name = 'Dan' AND c.last_name = 'Grigore'
+ORDER BY first_name;
+
+SELECT * FROM CLIENT c, CARD  cs, TRANSPORT t, VALIDATION v, CARD_TYPE ct
+where c.client_id = cs.client_id AND t.transport_id = v.transport_id AND v.card_id = cs.card_id AND ct.card_id = cs.card_id
+ORDER BY first_name;
+
+SELECT * FROM CLIENT c, CARD  cs
+where c.client_id = cs.client_id 
+ORDER BY first_name;
+
+SELECT * FROM CARD c, CLIENT cl WHERE c.client_id = cl.client_id AND cl.first_name = 'Dan' AND cl.last_name = 'Grigore';
+
+SELECT * FROM CARD c, CLIENT cl WHERE c.client_id = cl.client_id AND cl.first_name = 'Dan' AND cl.last_name = 'Grigore';
+
+UPDATE CARD SET card_money = card_money + 1 WHERE client_id = (SELECT client_id FROM CLIENT WHERE first_name = 'Alexandra' AND last_name = 'Gigica');
+
+ROLLBACK;
