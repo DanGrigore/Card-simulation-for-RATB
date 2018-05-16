221813d989ab1c5ff2ea4e34f1fb250e22a5e1ac
Nadstrif <grigoredanandrei@yahoo.com>
2018-05-16 22:29:55 +0300

    testing

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -3,6 +3,8 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Server {
@@ -29,10 +31,20 @@ public class Server {
         public void run() {
             try {
                 DataBase db = new DataBase();
-               // db.connect("SELECT * FROM CLIENT");
+                // db.connect("SELECT * FROM CLIENT");
 
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                out.writeObject(db.connect("SELECT * FROM CLIENT"));
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                Map welcome = new HashMap();
+                try {
+                    welcome = (HashMap) in.readObject();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                if (welcome.containsKey("afisare")) {
+                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                    out.writeObject(db.connect("SELECT * FROM CLIENT"));
+                }
             } catch (IOException e) {
 
             } finally {
--- src/connection/ClientConnection.java
+++ src/connection/ClientConnection.java
@@ -1,7 +1,11 @@
 package connection;
 
+import models.Client;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientConnection {
 
@@ -13,13 +17,30 @@ public class ClientConnection {
 
         // Make connection and initialize streams
         Socket socket = new Socket("localhost", 7777);
+        Map toServer = new HashMap();
+        String whatToDo = "afisare";
+        Client aux = new Client();
+        toServer.put(whatToDo,aux);
+        os = new ObjectOutputStream(socket.getOutputStream());
+        os.writeObject(toServer);
+
         in = new ObjectInputStream(socket.getInputStream());
 
-       // models.Client x = (models.Client) in.readObject();
-       // System.out.println(x.getFirstName() + " " + x.getLastName());
-        String className = in.readObject().getClass().getName();
-        System.out.println(className);
+        Object object = in.readObject();
+        String className = object.getClass().getName();
+
+        switch(className){
+            case "models.Client":
+                Client x = (Client) object;
+                System.out.println(x.getFirstName() + " " + x.getLastName());
+                break;
+            default:
+                System.out.println("Oops");
+        }
         in.close();
+        os.close();
+        toServer.clear();
+        socket.close();
     }
 
 
