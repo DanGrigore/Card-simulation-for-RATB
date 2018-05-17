a2ea00b86a258a1d8db4f018be892f25d134e599
Nadstrif <grigoredanandrei@yahoo.com>
2018-05-17 08:34:14 +0300

    db update

--- DataBase/DB_inserts.sql
+++ DataBase/DB_inserts.sql
@@ -2,11 +2,13 @@ INSERT INTO CLIENT(first_name,last_name) VALUES('Dan','Grigore');
 INSERT INTO CLIENT(first_name,last_name) VALUES('Andrei','Iancu');
 INSERT INTO CLIENT(first_name,last_name) VALUES('Alexandra','Gigica');
 INSERT INTO CLIENT(first_name,last_name) VALUES('Dan','Chiru');
+INSERT INTO CLIENT(first_name,last_name) VALUES('test1','test2');
 
 
 INSERT INTO CARD(card_money,expire_on,client_id) VALUES(23,null,3);
 INSERT INTO CARD(card_money,expire_on,client_id) VALUES(10,CURDATE() + INTERVAL 30 DAY,4);
 INSERT INTO CARD(card_money,expire_on,client_id) VALUES(10,STR_TO_DATE('20-05-2018', '%d-%m-%Y'),2);
+INSERT INTO CARD(card_money,expire_on,client_id) VALUES(50,null,7);
 
 INSERT INTO TRANSPORT(line,type,charge_per_trip) VALUES(41,'tramvai',1.3);
 INSERT INTO TRANSPORT(line,type,charge_per_trip) VALUES(336,'autobuz',1.3);
@@ -21,6 +23,13 @@ INSERT INTO VALIDATION(card_id,transport_id) VALUES(10,1);
 DELETE FROM CARD WHERE card_id = 12;
 
 DELETE FROM VALIDATION WHERE id = 3;
+
+SELECT * FROM CARD_TYPE;
+
+SELECT * FROM CLIENT;
+SELECT * FROM CARD;
+
+UPDATE CARD SET card_money = card_money + 1 WHERE client_id = (SELECT client_id FROM CLIENT WHERE first_name = 'Alexandra' AND last_name = 'Gigica');
 
 SELECT * FROM CLIENT;
 SELECT * FROM CARD;
